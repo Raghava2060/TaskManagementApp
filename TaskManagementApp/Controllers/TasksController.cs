2b982b4089da677de8f69ec17bb2dfea7252973c
Raghava <jalluriraghava@gmail.com>
2026-08-17 17:35:34 +0530

    assignment 2

--- TaskManagementApp/Controllers/TasksController.cs
+++ TaskManagementApp/Controllers/TasksController.cs
@@ -1,23 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskManagementApp.DTOs;
 using TaskManagementApp.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace TaskManagementApp.Controllers
 {
     public class TasksController : Controller
     {
         private readonly ITaskRepository _repository;
+        private readonly ILogger<TasksController> _logger;
 
-        public TasksController(ITaskRepository repository)
+        public TasksController(
+            ITaskRepository repository,
+            ILogger<TasksController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         // GET: /Tasks
         [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("User opened the Task List.");
             var tasks = _repository.GetAll();
+
+            _logger.LogInformation("Task list loaded Successfully.");
             return View(tasks);
         }
 
@@ -25,12 +33,15 @@ namespace TaskManagementApp.Controllers
         [HttpGet]
         public IActionResult Details(int id)
         {
+            _logger.LogInformation("User requested details for Task ID: {TaskId}", id);
             var task = _repository.GetById(id);
 
             if (task == null)
             {
+                _logger.LogWarning("Task ID: {TaskId} was not found.", id);
                 return NotFound();
             }
+            _logger.LogInformation("Task details loaded successfully for Task ID: {TaskId}", id);
 
             return View(task);
         }
@@ -39,6 +50,7 @@ namespace TaskManagementApp.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            _logger.LogInformation("User opened the Created Task page.");
             return View();
         }
 
@@ -47,13 +59,19 @@ namespace TaskManagementApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(TaskDto taskDto)
         {
+            _logger.LogInformation("User submitted a new task.");
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning("Created task failed because the submitted data is invalid.");
                 return View(taskDto);
             }
 
             _repository.Add(taskDto);
             _repository.Save();
+
+            _logger.LogInformation(
+                "Task created successfully. Task Description: {TaskDescription}",
+                taskDto.TaskDescription);
 
             return RedirectToAction(nameof(Index));
         }
@@ -62,10 +80,12 @@ namespace TaskManagementApp.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
+            _logger.LogInformation("User opened Edit page for Task ID: {TaskId}", id);
             var task = _repository.GetById(id);
 
             if (task == null)
             {
+                _logger.LogWarning("Task ID: {TaskId} was not found for editing.", id);
                 return NotFound();
             }
 
@@ -77,14 +97,24 @@ namespace TaskManagementApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(TaskDto taskDto)
         {
+            _logger.LogInformation(
+                "User submitted an update for Task ID: {TaskId}",
+                  taskDto.TaskId);
             if (!ModelState.IsValid)
             {
+                _logger.LogWarning(
+                     "Update failed for Task ID: {TaskId} because the submitted data is invalid.",
+                     taskDto.TaskId);
+
                 return View(taskDto);
             }
 
             _repository.Update(taskDto);
             _repository.Save();
 
+            _logger.LogInformation(
+               "Task ID: {TaskId} updated successfully.",
+                taskDto.TaskId);
             return RedirectToAction(nameof(Index));
         }
 
@@ -93,15 +123,25 @@ namespace TaskManagementApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
+            _logger.LogInformation(
+                 "User requested deletion of Task ID: {TaskId}",
+                  id);
             var task = _repository.GetById(id);
 
             if (task == null)
             {
+                _logger.LogWarning(
+                    "Task ID: {TaskId} was not found for deletion.",
+                    id);
                 return NotFound();
             }
 
             _repository.Delete(id);
             _repository.Save();
+
+            _logger.LogInformation(
+                "Task ID: {TaskId} deleted successfully.",
+                id);
 
             return RedirectToAction(nameof(Index));
         }
